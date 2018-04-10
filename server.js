f452472094ff4fdd9ee79194e4ff30b53bdf0c51
quinlayen <quinlayen@gmail.com>
2018-04-10 11:30:41 -1000

    migration tables made

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
 const PORT = process.env.PORT || 3000; 
 
 app.use(require('./routes'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.listen(PORT,()=>{
