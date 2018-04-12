aa2c015805c843b3738596f84b6b1a1300f50fa0
quinlayen <quinlayen@gmail.com>
2018-04-11 21:58:18 -1000

    tables generated

--- migrations/20180410103820_create_user_table.js
+++ migrations/20180410103820_create_user_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('user', function(table){
+        console.log(table)
+        table.increments('user_id');
+        table.string('email').notNullable();
+        table.string('password');
+        table.timestamps(true,true);
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.scheme.dropTable('user');
 };
