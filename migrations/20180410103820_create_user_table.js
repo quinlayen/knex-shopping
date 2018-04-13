4b9c5fb6071a2564114373e64bb7e56590e3b00c
quinlayen <quinlayen@gmail.com>
2018-04-12 21:09:26 -1000

    first query works

--- migrations/20180410103820_create_user_table.js
+++ migrations/20180410103820_create_user_table.js
@@ -1,7 +1,6 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('user', function(table){
-        console.log(table)
         table.increments('user_id');
         table.string('email').notNullable();
         table.string('password');
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'knex_shopping_user',
+        database:'knex_shopping'
+    }
+  })
 
 router.get('/users', (req,res)=>{
     res.send('Users Working')
 })
 
+router.get('/users/:user_id', (req,res)=>{
+    const num = Number(req.params.user_id);
+    knex.select('user_id').from('user')
+    .where({
+        user_id: num
+    })
+    .then( (data)=>{
+        console.log('data', data);
+    })
+})
 
 module.exports = router;
